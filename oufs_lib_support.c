98ee15011bf5a19ad11971c9f9431157b919f35e
Austin Gabe Scott <austin.g.scott-1@ou.edu>
2019-11-22 21:29:21 -0600

    changed address of operator to just the pointer for new_block inside a v_d_read_block

--- oufs_lib_support.c
+++ oufs_lib_support.c
@@ -651,7 +651,7 @@ BLOCK_REFERENCE oufs_allocate_new_block(BLOCK *master_block, BLOCK *new_block)
     BLOCK_REFERENCE front;
     front = master_block->content.master.unallocated_front;
     fprintf(stderr, "inside allocate_new_b front == %d\n", front);
-    virtual_disk_read_block(front, &new_block);
+    virtual_disk_read_block(front, new_block);
     fprintf(stderr, "inside allocate_new_b pointer &newblock is %d\n", new_block);
     /////
     //fprintf(stderr, "inside allocate_new_b new_block->next_block == %d\n", new_block->next_block);
