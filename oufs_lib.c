ba93b6a076f8070fbb478971e100a59b286e889b
Austin Gabe Scott <austin.g.scott-1@ou.edu>
2019-11-29 21:23:33 -0600

    changed fopen's cache preparation for link(). The blocks are not malloc'd with size DATA_BLOCK_SIZE

--- oufs_lib.c
+++ oufs_lib.c
@@ -543,7 +543,7 @@ OUFILE* oufs_fopen(char *cwd, char *path, char *mode)
         int count = 0;
         BLOCK_REFERENCE b;
         b = inode.content;
-        BLOCK *c = malloc(sizeof(BLOCK));
+        BLOCK *c = malloc(DATA_BLOCK_SIZE);
         virtual_disk_read_block(b, c);
         while (b != UNALLOCATED_BLOCK)
         {
@@ -624,7 +624,7 @@ OUFILE* oufs_fopen(char *cwd, char *path, char *mode)
             int count = 0;
             BLOCK_REFERENCE b;
             b = inode.content;
-            BLOCK *c = malloc(sizeof(BLOCK));
+            BLOCK *c = malloc(DATA_BLOCK_SIZE);
             
             while (b != UNALLOCATED_BLOCK)
             {
